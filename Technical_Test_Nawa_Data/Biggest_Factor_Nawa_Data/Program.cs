057fb1804e0b92218186b3965ef88b4aed558e49
Adnan <mursyidinadnan@gmail.com>
2020-06-23 02:06:45 +0700

    first

---
+++ Technical_Test_Nawa_Data/Biggest_Factor_Nawa_Data/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biggest_Factor_Nawa_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukan jumlah input: ");
+            int Input = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[Input];
+
+            for (int i = 0; i < Input; i++)
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukan Output: ");
+            int Output = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Output factor: ");
+            FungsiCariFactor(array, Output);
+            Console.ReadKey();
+        }
+
+        private static void FungsiCariFactor(int[] Array1, int Output)
+        {
+            int[] sub = new int[Array1.Length];
+            int temp = 0;
+            for (int i = 0; i < Array1.Length; i++)
+            {
+                for (int j = i, col = 0; j < Array1.Length; j++, col++)
+                {
+                    temp += Array1[j];
+                    sub[col] = Array1[j];
+                    if (temp == Output)
+                    {
+                        int total = 0;
+                        for (int k = 0; k < sub.Length; k++)
+                        {
+                            total += sub[k];
+                            Console.Write(sub[k].ToString() + " ");
+
+                            if (total == Output)
+                            {
+                                Console.Write("\n");
+                                break;
+                            }
+                        }
+                    }
+                    if (temp > Output)
+                    {
+                        Array.Clear(sub, 0, sub.Length);
+                        temp = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Technical_Test_Nawa_Data/Sort_Character_Nawa_Data/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Character_Nawa_Data
+{
+    class Program
+    {
+                
+        public static void Main()
+        {
+            int vCount = 0, cCount = 0;
+            Console.WriteLine("tulis: ");            
+            string str = Console.ReadLine();
+            str = str.ToLower();
+
+            string[] vowelsarray = new string[100];
+            string[] consonantarray = new string[100];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+                {
+                    vowelsarray[vCount] = str[i].ToString();
+                    Array.Sort(vowelsarray);
+                    vCount++;
+                }
+                else if (str[i] >= 'a' && str[i] <= 'z')
+                {
+                    consonantarray[cCount] = str[i].ToString();
+                    Array.Sort(consonantarray);
+                    cCount++;
+                }
+            }
+            
+            Console.WriteLine("Vowel Characters :");
+            Console.WriteLine("{0}", string.Join("", vowelsarray));            
+            Console.WriteLine("Consonant Characters :");
+            Console.WriteLine("{0}", string.Join("", consonantarray));
+            Console.ReadLine();
+        }
+
+    }
+}
